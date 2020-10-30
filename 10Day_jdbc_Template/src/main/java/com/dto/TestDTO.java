69512fbf36f5dd244f1535c92b99fb5bf14c6a88
velSeon <seonday28@gmail.com>
2020-10-30 09:42:55 +0900

    STUDY SPRING

---
+++ 10Day_jdbc_Template/src/main/java/com/dto/TestDTO.java
@@ -0,0 +1,42 @@
+package com.dto;
+
+public class TestDTO {
+int num;
+String username;
+String address;
+public int getNum() {
+	return num;
+}
+public void setNum(int num) {
+	this.num = num;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+@Override
+public String toString() {
+	return "TestDTO [num=" + num + ", username=" + username + ", address=" + address + "]";
+}
+public TestDTO(int num, String username, String address) {
+	super();
+	this.num = num;
+	this.username = username;
+	this.address = address;
+}
+public TestDTO() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+
+
+}
---
+++ 10Day_jdbc_Tx/src/main/java/testMain.java
@@ -0,0 +1,15 @@
+
+public class testMain {
+
+	public static void main(String[] args) {
+	TestDAO dao = new TestDAO();
+	
+	dao.insert(10,"김민수", "뉴욕");
+	
+	
+
+
+
+	
+}
+}
